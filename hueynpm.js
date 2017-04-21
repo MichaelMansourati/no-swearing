92d81cc73651d7d26f01de889f8cb24eec02bb6c
Michael Mansourati <mmansourati@gmail.com>
2017-04-21 22:53:35 +0000

    basic layout and sass implemented

--- hueynpm.js
+++ hueynpm.js
@@ -3,56 +3,25 @@ var dominant = require('huey/dominant')
 var palette = require('huey/palette')
 var huey = require('huey')
 var express = require("express")
+var ejs = require('ejs')
 var app = express()
+var nodemon = require('nodemon')
 var PORT = process.env.PORT || 8080
-var API500px = require('500px'),
-    api500px = new API500px('oWNQ7L5evkIbRg5ZUUXYiWdpAVdWhpQf3iRlZe0F');
-
-api500px.photos.getPopular({'sort': 'created_at', 'rpp': '100'},  function(error, results) {
-  if (error) {
-    throw err
-    // Error!
-    return;
-  }
-console.log(results)
-  // Do something
-});
 
 
-const img = './DSCF0562.jpg'
 
-let hex = []
-huey.palette(img, 4, function(error, palette, image) {
-  palette.forEach(function (rgb) {
-    hex.push(rgbToHex(rgb[0], rgb[1], rgb[2]))
-    console.log(rgbToHex(rgb[0], rgb[1], rgb[2]))
-  })
+// app.get("/", (req, res) => {
+//   res.render("index");
+// });
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/views'))
+app.use(express.static(__dirname + '/public'));;
+
+app.get('/', function(req, res) {
+  res.render('index')
 })
-
-function componentToHex(c) {
-    var hex = c.toString(16);
-    return hex.length == 1 ? "0" + hex : hex;
-}
-
-function rgbToHex(r, g, b) {
-    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-}
-
-app.get("/", (req, res) => {
-  res.send(`<!DOCTYPE html>
-  <head>
-    <link rel="stylesheet" href="/styles/hueynpm.css" type="text/css" </link>
-  </head>
-  <body>
-    Hello?
-    <img src=${img} height="42">
-    <div style="padding:40px;background-color:${hex[0]};"></div>
-    <div style="padding:40px;background-color:${hex[1]};"></div>
-    <div style="padding:40px;background-color:${hex[2]};"></div>
-    <div style="padding:40px;background-color:${hex[3]};"></div>
-  </body>`
-  );
-});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
